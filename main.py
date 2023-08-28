b0a0766b4cac5b5747799c4069fc76453458e863
kevinzeladacl <iam@kevinzelada.cl>
2023-08-28 11:07:40 -0400

    update

--- main.py
+++ main.py
@@ -32,35 +32,28 @@ def read_prompt(item:Item):
 
     agent_instance = Agent(api_key=codegpt_api_key, agent_id=codegpt_agent_id)
 
-    # prompt = "tell me short story about blue turtle"
-
     response = asyncio.run(agent_instance.completion(prompt, stream=True))
 
-    
-
-    
     return response
     
 
 
+# @app.post("/check/")
+# async def create_item(item: Item):
 
 
-@app.post("/check/")
-async def create_item(item: Item):
+#     print(item)
+
+#     result = num_tokens_from_string(item.text, "cl100k_base")
+
+#     return result
 
 
-    print(item)
-
-    result = num_tokens_from_string(item.text, "cl100k_base")
-
-    return result
-
-
-def num_tokens_from_string(string: str, encoding_name: str) -> int:
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.get_encoding(encoding_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
+# def num_tokens_from_string(string: str, encoding_name: str) -> int:
+#     """Returns the number of tokens in a text string."""
+#     encoding = tiktoken.get_encoding(encoding_name)
+#     num_tokens = len(encoding.encode(string))
+#     return num_tokens
 
 
 
